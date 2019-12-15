db6294a5304e696cb968f5089ca5ebb0a0b3fe69
dsg <vijayadinesh93@gmail.com>
2019-12-15 12:26:26 +0530

    2 mie problems

---
+++ 110. Balanced Binary Tree.py
@@ -0,0 +1,52 @@
+"""
+
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+Example 1:
+
+Given the following tree [3,9,20,null,null,15,7]:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Return true.
+
+Example 2:
+
+Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+Return false.
+
+"""
+
+
+def isBalanced(root):
+    if root is None:
+        return True
+
+    def height(root):
+
+        if root is None:
+            return True
+
+        return max(height(root.left), height(root.right)) + 1
+
+    lh = height(root.left)
+    rh = height(root.right)
+
+    if abs(lh - rh) <= 1 and isBalanced(root.left) and isBalanced(root.right):
+        return True
+
+    return False
